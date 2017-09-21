7212a7f957d17122c1e45c87e886efb02abf334f
Abhinav Singh <sudo.abhinav@gmail.com>
2017-09-21 21:49:37 +0530

    Change user model and add books app
     - Add fields and migration to user model
     - Add books app to track books per requirement

--- bookstudio/users/admin.py
+++ bookstudio/users/admin.py
@@ -33,7 +33,7 @@ class MyUserAdmin(AuthUserAdmin):
     form = MyUserChangeForm
     add_form = MyUserCreationForm
     fieldsets = (
-            ('User Profile', {'fields': ('name',)}),
+            ('User Profile', {'fields': ('fisrt_name', 'last_name')}),
     ) + AuthUserAdmin.fieldsets
-    list_display = ('username', 'name', 'is_superuser')
-    search_fields = ['name']
+    list_display = ('username', 'first_name', 'is_superuser')
+    search_fields = ['first_name']
--- bookstudio/users/models.py
+++ bookstudio/users/models.py
@@ -3,14 +3,23 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from django_countries.fields import CountryField
 
 
 @python_2_unicode_compatible
 class User(AbstractUser):
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    landmark = models.CharField(max_length=50, null=True, blank=True)
+    pincode = models.CharField(max_length=5, null=True, blank=True)
+    gender = models.CharField(max_length=8, null=True, blank=True, choices=GENDER_CHOICES)
+    country = CountryField(blank_label='(select country)', null=True, blank=True)
 
     def __str__(self):
         return self.username
