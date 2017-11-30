c0fdbec5087e98c70ef8fb0f43548c93973a3179
achintVII <achint43@gmail.com>
2017-12-01 01:06:10 +0530

    minor ui changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
+    
 ]
 
 # MIGRATIONS CONFIGURATION
